7bae964fe0b201820907bbe4517c8c3dc6b6d03e
fsasvari <frane.sasvari@gmail.com>
2018-05-06 09:48:44 +0200

    Prepare controllers

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,31 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * The http request instance
+     *
+     * @var \Illuminate\Http\Request
+     */
+    protected $request;
+
+    /**
+     * The response instance
+     *
+     * @var \Illuminate\Contracts\Routing\ResponseFactory
+     */
+    protected $response;
+
+    /**
+     * Create new controller instance
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Contracts\Routing\ResponseFactory  $response
+     * @return void
+     */
+    public function __construct(Request $request, Response $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
 }
---
+++ app/Http/Controllers/Web/Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    //
+}
