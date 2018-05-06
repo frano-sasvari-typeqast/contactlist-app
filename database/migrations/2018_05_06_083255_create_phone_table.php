925126f9574cd6c30a3993dcec3914ede1568da1
fsasvari <frane.sasvari@gmail.com>
2018-05-06 08:36:00 +0200

    Create migrations

---
+++ database/migrations/2018_05_06_083255_create_phone_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhoneTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('contact_id');
+            $table->string('label', 60);
+            $table->string('number', 30);
+            $table->timestamps();
+
+            $table->foreign('contact_id')
+                ->references('id')->on('contact')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone');
+    }
+}
