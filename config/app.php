b46183a8df5bda8df9436969212b69927abf32c2
fsasvari <frane.sasvari@gmail.com>
2018-05-06 15:47:48 +0200

    Cleanup config files

--- config/app.php
+++ config/app.php
@@ -148,17 +148,15 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
-         * Package Service Providers...
-         */
-
-        /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        /*
+         * Package Service Providers...
+         */
+        //
 
     ],
 
