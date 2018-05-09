b766d6a9f8de9216829e016f6097f42d10fe8f2f
fsasvari <frane.sasvari@gmail.com>
2018-05-09 22:25:42 +0200

    Add Faker Generator to CreatesApplication test trait

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -3,9 +3,17 @@
 namespace Tests;
 
 use Illuminate\Contracts\Console\Kernel;
+use Faker\Generator as Faker;
 
 trait CreatesApplication
 {
+    /**
+     * The faker generator instance
+     *
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
     /**
      * Creates the application.
      *
@@ -16,6 +24,8 @@ trait CreatesApplication
         $app = require __DIR__.'/../bootstrap/app.php';
 
         $app->make(Kernel::class)->bootstrap();
+
+        $this->faker = $app->make(Faker::class);
 
         return $app;
     }
