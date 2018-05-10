26644219cb0202d9c5daacb1dd29291271c48e6e
Frano <Frano@DESKTOP-FRANO.agroklub.lan>
2018-05-10 14:10:32 +0200

    Move contact searches to ContactSearch class

--- tests/Feature/Api/ContactIndexTest.php
+++ tests/Feature/Api/ContactIndexTest.php
@@ -65,48 +65,4 @@ class ContactTest extends TestCase
                 'lastname' => $contact->lastname,
             ]);
     }
-
-    /**
-     * Test contact search page by "name" parameter
-     *
-     * @return void
-     */
-    public function testContactSearchByName()
-    {
-        factory(Contact::class, 20)->create();
-
-        $contact = Contact::inRandomOrder()->first();
-
-        $response = $this->get('contacts?keyword='.$contact->firstname);
-
-        $response
-            ->assertStatus(200)
-            ->assertJsonFragment([
-                'firstname' => $contact->firstname,
-                'lastname' => $contact->lastname,
-                'email' => $contact->email,
-            ]);
-    }
-
-    /**
-     * Test contact search page by "email" parameter
-     *
-     * @return void
-     */
-    public function testContactSearchByEmail()
-    {
-        factory(Contact::class, 20)->create();
-
-        $contact = Contact::inRandomOrder()->first();
-
-        $response = $this->get('contacts?keyword='.$contact->email);
-
-        $response
-            ->assertStatus(200)
-            ->assertJsonFragment([
-                'firstname' => $contact->firstname,
-                'lastname' => $contact->lastname,
-                'email' => $contact->email,
-            ]);
-    }
 }
