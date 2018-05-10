360d11d799f0f0d5bb76b3e0fc3d0b6db9bdcbd6
Frano <Frano@DESKTOP-FRANO.agroklub.lan>
2018-05-10 10:43:02 +0200

    Create ContactCreate and ContactIndex tests

---
+++ tests/Feature/Api/ContactCreateTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+
+class ContactCreateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test contact show page
+     *
+     * @return void
+     */
+    public function testContactCreateSuccess()
+    {
+        $uploadAvatar = str_slug($this->faker->word).'.jpg';
+
+        $input = [
+            'firstname' => $this->faker->firstName,
+            'lastname' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'upload_avatar' => UploadedFile::fake()->image($uploadAvatar),
+            'is_favorite' => 1,
+        ];
+
+        $response = $this->json('POST', 'contacts', $input);
+
+        $response
+            ->assertStatus(201)
+            ->assertJsonFragment([
+                'firstname' => $input['firstname'],
+                'lastname' => $input['lastname'],
+            ]);
+    }
+
+    /**
+     * Test contact show page
+     *
+     * @return void
+     */
+    public function testContactCreateEmailValidationError()
+    {
+        $input = [
+            'firstname' => $this->faker->firstName,
+            'lastname' => $this->faker->lastName,
+            'email' => '',
+            'upload_avatar' => null,
+            'is_favorite' => 1,
+        ];
+
+        $response = $this->json('POST', 'contacts', $input);
+
+        $response
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('email');
+    }
+}
---
+++ tests/Feature/Api/ContactIndexTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Model\Contact;
+
+class ContactTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test contact index page
+     *
+     * @return void
+     */
+    public function testContactIndexPage()
+    {
+        factory(Contact::class, 50)->create();
+
+        $contacts = Contact::paginate(20);
+
+        $contact = $contacts->random();
+
+        $response = $this->get('contacts');
+
+        $response
+            ->assertSuccessful()
+            ->assertJsonFragment([
+                'from' => 1,
+                'to' => 20,
+                'total' => $contacts->total(),
+            ])
+            ->assertJsonFragment([
+                'firstname' => $contact->firstname,
+                'lastname' => $contact->lastname,
+            ]);
+    }
+
+    /**
+     * Test contact index page on pagination page 3
+     *
+     * @return void
+     */
+    public function testContactIndexPageOnPaginationPage3()
+    {
+        factory(Contact::class, 50)->create();
+
+        $contacts = Contact::paginate(20, ['*'], 'page', 3);
+
+        $contact = $contacts->random();
+
+        $response = $this->get('contacts?page=3');
+
+        $response
+            ->assertSuccessful()
+            ->assertJsonFragment([
+                'from' => 41,
+                'to' => 50,
+                'total' => $contacts->total(),
+            ])
+            ->assertJsonFragment([
+                'firstname' => $contact->firstname,
+                'lastname' => $contact->lastname,
+            ]);
+    }
+}
