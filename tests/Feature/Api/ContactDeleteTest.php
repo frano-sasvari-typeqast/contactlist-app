d5f0b544bac1bdabeea303b7f25e8c62e2a22f4b
Frano <Frano@DESKTOP-FRANO.agroklub.lan>
2018-05-10 11:14:29 +0200

    Finish ContactDelete test

--- tests/Feature/Api/ContactDeleteTest.php
+++ tests/Feature/Api/ContactDeleteTest.php
@@ -4,10 +4,37 @@ namespace Tests\Feature\Api;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Model\Contact;
 
 class ContactDeleteTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+    /**
+     * Test contact show page
+     *
+     * @return void
+     */
+    public function testContactDeleteSuccess()
+    {
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->json('DELETE', 'contacts/'.$contact->id);
+
+        $response
+            ->assertStatus(200);
+    }
+
+    /**
+     * Test contact show page
+     *
+     * @return void
+     */
+    public function testContactDeleteError()
+    {
+        $response = $this->json('DELETE', 'contacts/123123123');
+
+        $response
+            ->assertStatus(404);
+    }
 }
