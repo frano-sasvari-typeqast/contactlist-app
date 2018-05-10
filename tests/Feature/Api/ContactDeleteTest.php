3f4b0095b9a03abdd0997643f08bcea7853eb622
Frano <Frano@DESKTOP-FRANO.agroklub.lan>
2018-05-10 10:43:43 +0200

    Prepare ContactDelete test

---
+++ tests/Feature/Api/ContactDeleteTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ContactDeleteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    
+}
